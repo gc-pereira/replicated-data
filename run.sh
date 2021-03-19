9bda929cbeb65fe03f286b8cace816e2dd893b60
gc-pereira <carvalho.gabrielp@gmail.com>
2021-03-19 17:23:52 -0300

    shell script run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker build -t mysql-image -f db/Dockerfile .
+docker run -d --rm --name mysql-container mysql-image
+python3 -m venv ~/Desktop/replicated-data/.venv
+source .venv/bin/activate
+pip install -r main/requirements.txt
